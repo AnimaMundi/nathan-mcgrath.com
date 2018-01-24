52107eab9a9fc5e32d358fe8a2c06758fa335d07
Nathan Mc Grath <contact@nathan-mcgrath.com>
2018-01-24 23:56:15 +0000

    fix alignment issues

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -3,6 +3,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'nmg-experience',
   templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExperienceComponent { }
